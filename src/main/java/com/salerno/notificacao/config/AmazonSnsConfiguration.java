4aa3be06d6ddb1c8136025837d84b8946d398a09
jcsalerno <contato.jcsalerno@gmail.com>
2025-04-21 00:27:39 -0300

    fix: correção de importes inadequados

--- src/main/java/com/salerno/notificacao/config/AmazonSnsConfiguration.java
+++ src/main/java/com/salerno/notificacao/config/AmazonSnsConfiguration.java
@@ -8,8 +8,6 @@ import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
 
 @Configuration
 public class AmazonSnsConfiguration {
